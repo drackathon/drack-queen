c678bba2f6300917d6f6d8e86eab82764da6b0e2
Benjamin Friedrich <benjamin.friedrich@zalando.de>
2016-02-12 19:34:53 +0100

    intial project setup

---
+++ sample-app/src/main/java/org/drack/hackathon/App.java
@@ -0,0 +1,13 @@
+package org.drack.hackathon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
