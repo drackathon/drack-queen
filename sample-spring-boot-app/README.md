80e9f6a151c303fb6c333ef3d671664dc66b18b1
Benjamin Friedrich <friedrich.benjamin@gmail.com>
2016-02-20 23:32:38 +0100

    added docker related configuration

--- sample-spring-boot-app/README.md
+++ sample-spring-boot-app/README.md
@@ -2,7 +2,15 @@
 
 
 ```
-$ docker-compose up -d # start database
+$ docker-compose start database # start database
 $ psql.sh # connect to test db (user + password = postgres)
 $ export DATABASE_IP=$(docker-machine ip <machine name>)
+```
+
+https://github.com/spotify/docker-maven-plugin
+mvn clean package docker:build
+
+## start app
+```
+$ docker-compose up -d
 ```
--- sample-spring-boot-app/src/test/java/org/drack/hackathon/PetsServiceTest.java
+++ sample-spring-boot-app/src/test/java/org/drack/hackathon/PetsServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration                                       // set up application context
 @SpringApplicationConfiguration(classes = App.class)       // set up how to load or configure application context
+@ActiveProfiles("development")
 public class PetsServiceTest {
     private MockMvc mockMvc;
 
